9814cc708676aa5e61d22e9ebca65ae94700ae50
sidhu-0408 <sidhusrini18@gmail.com>
2025-11-04 23:03:35 +0530

    Day 2: Part-1 Class & Object

---
+++ src/com/sidhu/oops/Car.java
@@ -0,0 +1,10 @@
+package com.sidhu.oops;
+
+public class Car {
+    String brand;
+    int year;
+
+    void start(){
+        System.out.println(brand + " Car is started..");
+    }
+}
---
+++ src/com/sidhu/oops/Main.java
@@ -0,0 +1,10 @@
+package com.sidhu.oops;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.brand="BMW";
+        car1.year = 2025;
+        car1.start();
+    }
+}
